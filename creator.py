a9b91b6002859a9a3461302ac5b32e653bdf4ab5
Sudo-Yotsugi <ivandesert00@gmail.com>
2026-01-08 22:36:03 +0800

    changed to auto scan directories

--- creator.py
+++ creator.py
@@ -6,27 +6,32 @@ def get_hardware_key(MAC):
     hash_digest = hashlib.sha256(byte_key).digest()
     return base64.urlsafe_b64encode(hash_digest)
 
-def create_sentinel_file():
+def create_aegis_file():
     MAC = uuid.getnode()
     MAC_key = get_hardware_key(MAC)
     cipher = Fernet(MAC_key)
 
-    file_name = input("Enter name of file: ")
+    file_name = input("Enter name of file: ").strip()
+    if not file_name.endswith(".aegis"):
+        file_name += ".aegis"
+
     secret_data = input("Enter data to lock: ")
     
     encrypted_data = cipher.encrypt(secret_data.encode())  
 
-    header = b"SENTINEL-V1:"
+    header = b"AEGIS-V1:"
+    extension = b".aegis"
 
-    with open(file_name, "wb") as f:
+    with open(file_name, "wb") as f:    
         f.write(header)
         f.write(encrypted_data)
+        f.write(extension)
 
     print(f"\n Successfully created {file_name}")
     print("This file can now only be unlocked by your MAC")
 
 if __name__ == "__main__":
-    create_sentinel_file()
+    create_aegis_file()
 
     
 
--- reader.py
+++ reader.py
@@ -6,6 +6,31 @@ def get_hardware_key(MAC):
     hash_digest = hashlib.sha256(byte_key).digest()
     return base64.urlsafe_b64encode(hash_digest)
 
+def get_common_file_locations():
+    home = os.path.expanduser("~")
+
+    common_locations = [
+        os.path.join(home, "Documents"),
+        os.path.join(home, "Desktop"),
+        os.path.join(home, "Downloads"),
+        os.getcwd()
+    ]
+
+    found_files = []
+
+    print("\nScanning common file locations for Aegis files")
+
+    for location in common_locations:
+        if os.path.exists(location):
+            for root, dirs, files in os.walk(location):
+                for file in files:
+                    if file.endswith(".aegis"):
+                        full_path = os.path.join(root, file)
+                        found_files.append(full_path)
+    
+    return found_files
+
+
 def self_destruct(file_name):
     print("Unauthorized user detected. Commencing Data Shredding \n")
     try:
@@ -20,16 +45,35 @@ def self_destruct(file_name):
     sys.exit
         
 def main():
-    file_name = input("Enter file name: ")
 
-    if not os.path.exists(file_name):
-        print("Error not found")
+    aegis_files = get_common_file_locations()
+    if not aegis_files:
+        print("No aegis files were found in your device.")
+        print("Move Aegis files into either Documents, Desktop, or Downloads")
+
+    print("\n" + "="*40)
+    print("     AEGIS FILE EXPLORER     ")
+    print("="*40)
+    print("")
+
+    for i, path in enumerate(aegis_files):
+        print(f"[{i}] {os.path.basename(path)} \nLocation: {path}\n" )
+    print("="*50)
+
+    try:
+        choice = int(input("Enter number of Aegis file to open: "))
+        open_file = aegis_files[choice]
+
+    except Exception as e:
+        print("Invalid selection")
         return
-    
-    with open(file_name, "rb") as f:
+
+    with open(open_file, "rb") as f:
         full_content = f.read()
 
-    header = b"SENTINEL-V1:"
+    header = b"AEGIS-V1:"
+    extension = b".aegis"
+
     if not full_content.startswith(header):
         print("Error: invalid file format")
         return
@@ -45,12 +89,12 @@ def main():
         decrypted_bytes = cipher.decrypt(encrypted_payload)
       
         print(f"\n--- ACCESS GRANTED ---")
-        print(f"DATA: {decrypted_bytes.decode()}")
+        print(f"CONTENTS: {decrypted_bytes.decode()}")
         input("\nPress Enter to close the vault.")
     
     except InvalidToken:
        
-        self_destruct(file_name)
+        self_destruct(open_file)
 
 if __name__ == "__main__":
     main()
